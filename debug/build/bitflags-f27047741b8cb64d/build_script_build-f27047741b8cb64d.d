fcaeb37e73c0ebb478512745ce538cca4d2bfe2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-14 14:20:42 +0000

    Deploy Kloenk/collectd-veconnect to github.com/Kloenk/collectd-veconnect.git:gh-pages

---
+++ debug/build/bitflags-f27047741b8cb64d/build_script_build-f27047741b8cb64d.d
@@ -0,0 +1,5 @@
+/home/travis/build/Kloenk/collectd-veconnect/public/debug/build/bitflags-f27047741b8cb64d/build_script_build-f27047741b8cb64d: /home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/build.rs
+
+/home/travis/build/Kloenk/collectd-veconnect/public/debug/build/bitflags-f27047741b8cb64d/build_script_build-f27047741b8cb64d.d: /home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/build.rs
+
+/home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/build.rs:
---
+++ debug/deps/bitflags-40252bd9dcc43248.d
@@ -0,0 +1,5 @@
+/home/travis/build/Kloenk/collectd-veconnect/public/debug/deps/bitflags-40252bd9dcc43248.rmeta: /home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/src/lib.rs
+
+/home/travis/build/Kloenk/collectd-veconnect/public/debug/deps/bitflags-40252bd9dcc43248.d: /home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/src/lib.rs
+
+/home/travis/.cargo/registry/src/github.com-1ecc6299db9ec823/bitflags-1.2.1/src/lib.rs:
